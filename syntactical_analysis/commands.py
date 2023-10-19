cb6ba6bc2bfd6f2785ea4f5a09c8ac7885c35818
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-19 16:13:41 +0300

    Новые команды

---
+++ syntactical_analysis/commands.py
@@ -0,0 +1,29 @@
+class AssignmentCommand(object):
+    def __init__(self, target: str, source: str):
+        self.target = target
+        self.source = source
+
+    @classmethod
+    def create(cls, target: str, source: str):
+        command = cls(target=target, source=source)
+        commands.append(command)
+
+    def __str__(self):
+        return f'{self.target} = {self.source}'
+
+
+class ConditionCommand(object):
+    def __init__(self, cond: str, goto_command_ind: int = None):
+        self.cond = cond
+        self.goto_command_ind = goto_command_ind
+
+    @classmethod
+    def create(cls, cond: str, goto_command_ind: int = None):
+        command = cls(cond, goto_command_ind)
+        commands.append(command)
+
+    def __str__(self):
+        return f'if {self.cond} goto {self.goto_command_ind}'
+
+
+commands = []
