e390bd01c237a3e51c1e1b0c7b81d7695c939bfe
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-20 21:27:04 +0300

    Более неактуально

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from lexical_analysis import LexicalAnalyzer
 from syntactical_analysis import SyntacticalAnalyzer
 
-DEBUG = False
 
 lexical_analyzer = LexicalAnalyzer()
 lexical_analyzer.analyze()
 lexical_analyzer.write()
 
 syntactical_analyzer = SyntacticalAnalyzer(lexical_analyzer.tokens)
-
-if DEBUG:
-    try:
-        syntactical_analyzer.analyze()
-    except Exception:
-        pass
-
-    syntactical_analyzer.write()
-else:
-    syntactical_analyzer.analyze()
+syntactical_analyzer.analyze()
+syntactical_analyzer.write()
